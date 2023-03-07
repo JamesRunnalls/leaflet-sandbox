444d97a682d404e30b244c8649f30e3c1cd9fe91
JamesRunnalls <james.runnalls@eawag.ch>
2023-03-07 16:49:36 +0100

    Add float geotiff

--- src/components/leaflet/basemap.jsx
+++ src/components/leaflet/basemap.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import L from "leaflet";
 import "./leaflet_geotiff";
+import "./leaflet_floatgeotiff";
 import "./leaflet_colorpicker";
 import "./leaflet_streamlines";
 import "./leaflet_vectorfield";
@@ -13,6 +14,26 @@ class Basemap extends Component {
       responseType: "arraybuffer",
     });
     return L.geotiff(data, {}).addTo(this.map);
+  };
+
+  addFloatGeotiff = async (url) => {
+    var { data } = await axios.get(url, {
+      responseType: "arraybuffer",
+    });
+    return L.floatgeotiff(data, {
+      min: 0,
+      max: 7,
+      palette: [
+        { color: [0, 0, 128], point: 0 },
+        { color: [51, 102, 255], point: 0.142857142857143 },
+        { color: [0, 176, 220], point: 0.285714285714286 },
+        { color: [0, 153, 51], point: 0.428571428571429 },
+        { color: [255, 255, 91], point: 0.571428571428571 },
+        { color: [230, 51, 0], point: 0.714285714285714 },
+        { color: [204, 0, 0], point: 0.857142857142857 },
+        { color: [128, 0, 0], point: 1 },
+      ],
+    }).addTo(this.map);
   };
 
   addStreamlines = async (url) => {
@@ -32,7 +53,7 @@ class Basemap extends Component {
 
   async componentDidMount() {
     this.store = {};
-    var center = [46.5, 6.67];
+    var center = [46.5, 6.57];
     var zoom = 11;
     this.map = L.map("map", {
       preferCanvas: true,
@@ -49,8 +70,12 @@ class Basemap extends Component {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
 
-    this.geotiff = this.addGeotiff(
+    /**this.geotiff = this.addGeotiff(
       "https://snowlines-geotiff.s3.eu-central-1.amazonaws.com/RGB_L2SNOW_reproj_idepix_subset_S3B_OL_1_EFR____20201227T101107_20201227T101407_20201227T121946_0179_047_179_2160_LN1_O_NR_002.SEN3.tif"
+    );**/
+
+    this.floatgeotiff = this.addFloatGeotiff(
+      "https://datalakes-eawag.s3.eu-central-1.amazonaws.com/leaflet-sandbox/s2_tsm_singleband_float.tiff"
     );
 
     this.streamline = this.addStreamlines(
